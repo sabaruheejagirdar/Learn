08d94dbfafd498a84133144827a64419130b6ca0
sabaruheejagirdar <sabaruheejagirdar@gmail.com>
2023-09-04 23:57:53 -0600

    Generators

--- Learn/JavaScript/Practise/1_Basics/Day5/js5_5Generators.js
+++ Learn/JavaScript/Practise/1_Basics/Day5/js5_5Generators.js
@@ -1,3 +1,5 @@
+
+function* genNumbers() {
 const x = yield 1;
 yield x + 2;
 yield 3;
@@ -7,4 +9,4 @@ const generatorObj = genNumbers();
 console.log(generatorObj.next().value);
 console.log(generatorObj.next(3).value);
 console.log(generatorObj.return(7).value);
-console.
+console.log(generatorObj.next().value);
